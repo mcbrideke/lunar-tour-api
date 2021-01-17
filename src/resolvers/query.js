b4cdd540a1280b4c0aebc283280442399dc3b6ad
unknown <kevj.m13@gmail.com>
2021-01-16 21:45:17 -0500

    additions

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,3 +1,46 @@
-export const hello = (args, context) => {
-    return "Your GraphQL API is now LIVE!🎈 "
-}
+import dynamodb from "../../libs/dynamodb-lib";
+
+export const getAllListings = async (args, context) => {
+  const params = {
+    TableName: process.env.ListingsDB || "dev-lunar-listings",
+  };
+
+  try {
+    const result = await dynamodb.scan(params);
+
+    if (result.Items.length === 0) {
+      return "You have no listings";
+    } else {
+      return result.Items.map((i) => ({
+        listingId: i.listingId,
+        coverPhoto: i.coverPhoto,
+        listingName: i.listingName,
+        listingDescription: i.listingDescription,
+        listingType: i.listingType.map((m) => ({
+          name: m,
+        })),
+        listingLocation: i.listingLocation,
+        listingActivities: i.listingActivities.map((k) => ({
+          name: k,
+        })),
+        specialType: i.specialType,
+        specialAmount: i.specialAmount,
+        rating: i.rating,
+        guide: {
+          Name: i.guide.name,
+          Bio: i.guide.bio,
+          Avatar: i.guide.avatar,
+        },
+        price: i.price,
+        numberOfDays: i.numberOfDays,
+      }));
+    }
+
+    // return result;
+  } catch (e) {
+    return {
+      message: e.message,
+      code: "500x",
+    };
+  }
+};
--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,39 @@
 const schema = `
 
-
-
-"""
-A hello world Query
-"""
-type Query {
-     hello: String!
+type ListingType {
+  name: String
 }
 
+type ListingActivities {
+  name: String
+}
+
+type Guide {
+  Name: String
+  Bio: String
+  Avatar: String
+}
+
+type Listing {
+     listingId: String
+     coverPhoto: String
+     listingName: String
+     listingDescription: String
+     listingType: [ListingType]
+     listingLocation: String
+     listingActivities: [ListingActivities]
+     specialType: String
+     specialAmount: Int
+     rating: Int
+     guide: Guide
+     price: String
+     numberOfDays: Int
+
+  }
+
+type Query {
+    getAllListings: [Listing]
+  }
 `
 
-export {schema}
+export {schema};
